aaf07af16f89277b75756544c823c9555c05e1a6
logan.gough26 <logan.gough26@d155.org>
2024-02-02 11:26:38 -0600

    Added JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 import javax.swing.*;
 
 public class Main {
-    JFrame frame = new JFrame();
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+
+        JPanel panel = new JPanel();
+
+        JLabel label = new JLabel("Today you will take an IQ test");
+
+        ImageIcon icon = new ImageIcon();
+
+        JOptionPane.showMessageDialog(null, "How old are you?", "IQ test", 1, icon);
 
 
+
+//        frame.add(panel);
+//
+//        frame.setVisible(true);
+
+
+    }
 }
