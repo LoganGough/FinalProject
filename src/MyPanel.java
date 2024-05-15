0c468353b8d83e004e8e1ff6b2a71d4db7056aaa
logan.gough26 <logan.gough26@d155.org>
2024-05-15 11:14:58 -0500

    did through stage 3 level 4

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,197 +1,279 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class MyPanel extends JPanel
-{
+public class MyPanel extends JPanel {
     Walls[] balls = new Walls[10];
     Walls[] laser = new Walls[2];
 
+
     static Font mainFont = new Font("Sanserif", Font.PLAIN, 30);
-boolean blah = false;
-int i = 0;
-int o = 0;
-double k = 0;
-int level = 0;
-String timer = "";
+    static  Font bigFont = new Font("bigFont", Font.BOLD, 100);
+    boolean blah = false;
+
+    boolean restart = true;
+    int i = 0;
+    int o = 0;
+    double k = 0;
+
+    double current;
+    double previous = 0;
+    int level = 0;
+    String timer = "";
     public MyPanel() {
         setBackground(Color.BLACK);
-        for(int k = 0; k < balls.length;k++){
+        for (int k = 0; k < balls.length; k++) {
             balls[k] = new Walls(300, 550);
         }
         laser[0] = new Walls(100, 150, 500);
-        laser[1] = new Walls(100, 100,  10, 500);
+        laser[1] = new Walls(100, 100, 10, 500);
         setFocusable(true);
+
 
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 System.out.println(e.getKeyCode());
-                if(e.getKeyCode() == 37){
-                    if(o%2==1) {
+                if (e.getKeyCode() == 37) {
+                    if (o % 2 == 1) {
                         balls[i].moveLeft();
                     }
                 }
 
-                if(e.getKeyCode() == 39){
-                    if(o%2==1) {
+
+                if (e.getKeyCode() == 39) {
+                    if (o % 2 == 1) {
                         balls[i].moveRight();
                     }
                 }
-//                if(e.getKeyCode()==38){
-//                    balls[i].boost();
-//                }
-                if(e.getKeyCode() == 36){
-                    for(int j = 0;j<20;j++) {
+                if (e.getKeyCode() == 36) {
+                    for (int j = 0; j < 20; j++) {
                         balls[i].slow();
                     }
                 }
-                if(e.getKeyCode() == 10){
-                    i++;
+                if (e.getKeyCode() == 10) {
+                    int red = (int)(Math.random()*255+1);
+                    int green = (int)(Math.random()*255+1);
+                    int blue = (int)(Math.random()*255+1);
+                    Color rgb = new Color(red, green, blue);
+                    setBackground(rgb);
                 }
-                if(e.getKeyCode()==8){
-                    i--;
+                if (e.getKeyCode() == 8) {
+                    setBackground(Color.black);
                 }
-                if(e.getKeyCode() == 32){
+                if (e.getKeyCode() == 32) {
                     o++;
                 }
+                if(e.getKeyCode() == 82){
+                    previous = k;
+                    level = 0;
+                    k = 0;
+                    i = 0;
+                    setBackground(Color.black);
+
+                }
+
 
             }
         });
     }
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.WHITE);
-        g.fillRect(100, 100, 10, 500);
-        g.fillRect(100, 600, 500, 10);
-        g.fillRect(590, 100, 10, 500);
-        g.fillRect(100, 100, 100, 10);
-        g.fillRect(350, 100, 250, 10);
-        g.fillRect(100, 400, 300, 10);
-        g.fillRect(500, 400, 100, 10);
 
-        if(i < 4) {
-            k += .012;
+
+            g.setColor(Color.WHITE);
+            g.fillRect(100, 100, 10, 500);
+            g.fillRect(100, 600, 500, 10);
+            g.fillRect(590, 100, 10, 500);
+            g.fillRect(100, 100, 100, 10);
+            g.fillRect(350, 100, 250, 10);
+            g.fillRect(100, 400, 300, 10);
+            g.fillRect(500, 400, 100, 10);
+            if (i < 4) {
+                if (level != 4)
+                    k += .016;
+
+            }
+
+
+            timer = "" + (int) (k);
+            g.setFont(mainFont);
+            String message = "Stage " + (level + 1) + "\n" + "level " + (i + 1);
+            g.drawString(timer, 700, 50);
+            g.drawString(message, 275, 50);
+
+
+
+                balls[i].draw(g);
+                balls[i].collision();
+                balls[i].level1Restart();
+                balls[i].level1RestartAgain();
+                blah = balls[i].restart();
+                balls[i].moves();
+                if (level == 1) {
+                    laser[0].draw2(g);
+                    laser[0].laser();
+                    int ballY = balls[i].getY();
+                    int laserY = laser[0].getY();
+
+
+                    if (o % 2 == 0) {
+
+
+                    } else if ((ballY == laserY) || ballY == laserY + 1 || ballY == laserY - 1) {
+                        balls[i].reset();
+                    }
+                }
+
+
+                if (blah) {
+                    i++;
+                    blah = false;
+                }
+                if (i == 1) {
+                    g.fillRect(100, 200, 100, 10);
+                    g.fillRect(300, 200, 300, 10);
+
+
+                    balls[i].level2Restart();
+                }
+                if (i == 2) {
+
+
+                    g.fillRect(100, 200, 100, 10);
+                    g.fillRect(300, 200, 300, 10);
+                    g.fillRect(100, 300, 400, 10);
+                    balls[i].level2Restart();
+                    balls[i].level3Restart();
+                }
+                if (i == 3) {
+
+
+                    g.fillRect(100, 200, 100, 10);
+                    g.fillRect(300, 200, 300, 10);
+                    g.fillRect(100, 300, 400, 10);
+                    g.fillRect(200, 500, 400, 10);
+                    balls[i].level2Restart();
+                    balls[i].level3Restart();
+                    balls[i].level4Restart();
+
+
+                }
+                if (i == 4) {
+                    level++;
+                    i = 0;
+
+
+                }
+                if (o % 2 == 0) {
+                    balls[i].pause();
+
+
+                }
+                if (level == 2) {
+                    laser[0].draw2(g);
+                    laser[0].laser();
+                    int ballX = balls[i].getX();
+                    int laserX = laser[1].getX();
+                    laser[1].draw3(g);
+                    laser[1].laser2();
+
+
+                    int ballY = balls[i].getY();
+                    int laserY = laser[0].getY();
+
+
+                    if (o % 2 == 0) {
+
+
+                    } else if ((ballY == laserY) || ballY == laserY + 1 || ballY == laserY - 1) {
+                        balls[i].reset();
+                    }
+                    if (o % 2 == 0) {
+
+
+                    } else if (ballX == laserX || ballX == laserX - 1 || ballX == laserX + 1 ||
+                            ballX == laserX + 2 || ballX == laserX - 2 || ballX == laserX + 3 || ballX == laserX - 3 ||
+                            ballX == laserX + 4 || ballX == laserX - 4 || ballX == laserX + 5 ||
+                            ballX == laserX - 5 || ballX == laserX + 6 || ballX == laserX - 6 ||
+                            ballX == laserX + 7 || ballX == laserX - 7 || ballX == laserX + 8 ||
+                            ballX == laserX - 8 || ballX == laserX + 9 || ballX == laserX - 9 ||
+                            ballX == laserX + 10 || ballX == laserX - 10 || ballX == laserX + 11 ||
+                            ballX == laserX - 11 || ballX == laserX + 12 || ballX == laserX - 12 ||
+                            ballX == laserX + 13 || ballX == laserX - 13 || ballX == laserX + 14 ||
+                            ballX == laserX - 14 || ballX == laserX + 15 || ballX == laserX - 15) {
+                        balls[i].reset();
+                    }
+                }
+                if (level == 3) {
+                    laser[0].draw2(g);
+                    laser[0].laserFast();
+                    int ballX = balls[i].getX();
+                    int laserX = laser[1].getX();
+                    laser[1].draw3(g);
+                    laser[1].laser2Fast();
+
+
+                    int ballY = balls[i].getY();
+                    int laserY = laser[0].getY();
+
+
+                    if (o % 2 == 0) {
+
+
+                    } else if ((ballY == laserY) || ballY == laserY + 1 || ballY == laserY - 1) {
+                        balls[i].reset();
+                    }
+                    if (o % 2 == 0) {
+
+
+                    } else if (ballX == laserX || ballX == laserX - 1 || ballX == laserX + 1 ||
+                            ballX == laserX + 2 || ballX == laserX - 2 || ballX == laserX + 3 || ballX == laserX - 3 ||
+                            ballX == laserX + 4 || ballX == laserX - 4 || ballX == laserX + 5 ||
+                            ballX == laserX - 5 || ballX == laserX + 6 || ballX == laserX - 6 ||
+                            ballX == laserX + 7 || ballX == laserX - 7 || ballX == laserX + 8 ||
+                            ballX == laserX - 8 || ballX == laserX + 9 || ballX == laserX - 9 ||
+                            ballX == laserX + 10 || ballX == laserX - 10 || ballX == laserX + 11 ||
+                            ballX == laserX - 11 || ballX == laserX + 12 || ballX == laserX - 12 ||
+                            ballX == laserX + 13 || ballX == laserX - 13 || ballX == laserX + 14 ||
+                            ballX == laserX - 14 || ballX == laserX + 15 || ballX == laserX - 15) {
+                        balls[i].reset();
+                    }
+
+                }
+                if (level == 4 && i == 0) {
+
+                    String jo = "" + (int) (k);
+                    g.setColor(Color.black);
+
+//                    if(k < previous){
+//                        previous = k;
+//                        String temp = "High Score: " + previous;
+//                        g.drawString(temp, 350, 250);
+//                    }
+//                    else{
+//                        g.drawString(jo, 350, 250);
+//                    }
+
+                    g.drawString("you're done", 350, 350);
+                    g.drawString(jo, 350, 250);
+                    g.setFont(bigFont);
+                    setBackground(Color.white);
+
+
+                }
+
+
+
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        timer = "" + k;
-        g.setFont(mainFont);
-        String message = "Stage " + (level + 1) + "\n" + "level " + (i + 1);
-        g.drawString(timer,700,50);
-        g.drawString(message, 275, 50);
-
-
-
-
-            balls[i].draw(g);
-            balls[i].collision();
-            balls[i].level1Restart();
-            balls[i].level1RestartAgain();
-            blah = balls[i].restart();
-        balls[i].moves();
-        if(level == 1) {
-            laser[0].draw2(g);
-            laser[0].laser();
-            int ballY =  balls[i].getY();
-            int laserY = laser[0].getY();
-            int ballX = balls[i].getX();
-            int laserX = laser[1].getX();
-
-
-            if(o%2==0){
-
-            }
-
-
-            else if((ballY == laserY)|| ballY == laserY + 1 || ballY == laserY - 1) {
-                balls[i].reset();
-            }
-        }
-
-            if(blah){
-                i++;
-                blah = false;
-            }
-            if(i==1){
-                g.fillRect(100, 200, 100, 10);
-                g.fillRect(300, 200, 300, 10);
-
-                balls[i].level2Restart();
-            }
-            if(i==2){
-
-                g.fillRect(100, 200, 100, 10);
-                g.fillRect(300, 200, 300, 10);
-                g.fillRect(100,300, 400,10);
-                balls[i].level2Restart();
-                balls[i].level3Restart();
-            }
-            if(i==3){
-
-
-                g.fillRect(100, 200, 100, 10);
-                g.fillRect(300, 200, 300, 10);
-                g.fillRect(100,300, 400,10);
-                g.fillRect(200, 500, 400, 10);
-                balls[i].level2Restart();
-                balls[i].level3Restart();
-                balls[i].level4Restart();
-            }
-            if(i==4){
-                level++;
-                i = 0;
-            }
-            if(o%2==0){
-                balls[i].pause();
-
-            }
-            if(level == 2){
-                laser[0].draw2(g);
-                laser[0].laser();
-                int ballX =  balls[i].getX();
-                int laserX = laser[1].getX();
-                laser[1].draw3(g);
-                laser[1].laser2();
-
-
-                int ballY =  balls[i].getY();
-                int laserY = laser[0].getY();
-
-
-                if(o%2==0){
-
-                }
-
-
-                else if((ballY == laserY)|| ballY == laserY + 1 || ballY == laserY - 1) {
-                    balls[i].reset();
-                }
-                if(o%2==0){
-
-                }
-                else if(ballX == laserX || ballX == laserX -1 || ballX == laserX + 1
-                        || ballX == laserX + 2 || ballX == laserX - 2 || ballX == laserX + 3 || ballX == laserX - 3
-                        || ballX == laserX + 4 || ballX == laserX - 4  || ballX == laserX + 5
-                        || ballX == laserX - 5 || ballX == laserX + 6 || ballX == laserX - 6
-                        || ballX == laserX + 7 || ballX == laserX - 7  || ballX == laserX + 8
-                        || ballX == laserX - 8 || ballX == laserX + 9 || ballX == laserX - 9
-                        || ballX == laserX + 10 || ballX == laserX - 10  || ballX == laserX + 11
-                        || ballX == laserX - 11 || ballX == laserX + 12 || ballX == laserX - 12
-                        || ballX == laserX + 13 || ballX == laserX - 13  || ballX == laserX + 14
-                        || ballX == laserX - 14 || ballX == laserX + 15 || ballX == laserX - 15){
-                    balls[i].reset();
-                }
-            }
-            
-
-
-
-            try {
-                Thread.sleep(10);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            repaint();
-        }
+        repaint();
+    }
 
 
 }
+
+
--- src/Walls.java
+++ src/Walls.java
@@ -5,202 +5,225 @@ import java.awt.event.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class Walls
-{
+
+public class Walls {
+
 
     public static boolean up = true;
     private int size;
     private Color color;
     public static boolean side = true;
-    private int xspeed;
     private int height;
-    private int yspeed;
     private int x, y;
-    private int centerx, centery;
 
 
-
-
-    public Walls(int x, int y){
-        centerx = x + size;
-        centery = y + size;
+    public Walls(int x, int y) {
         this.x = x;
         this.y = y;
         size = 25;
-        xspeed = 1;
-        yspeed = 1;
-        int random1 = (int)(Math.random()*200)+1;
-        int random2 = (int)(Math.random()*200)+1;
-        int random3 = (int)(Math.random()*200)+1;
         color = new Color(255, 255, 255);
     }
-    public Walls(int x, int y, int width){
+    public Walls(int x, int y, int width) {
         this.x = x;
         this.y = y;
         this.size = width;
     }
-    public Walls(int x, int y, int width, int height){
+    public Walls(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.size = width;
         this.height = height;
     }
-    public void draw(Graphics g2){
+    public void draw(Graphics g2) {
+
 
         g2.setColor(color);
-        g2.fillOval(x,y,size,size);
+        g2.fillOval(x, y, size, size);
+
 
     }
-    public void draw2(Graphics g2){
+    public void draw2(Graphics g2) {
         g2.setColor(Color.red);
-        g2.fillRect(x,y,size,10);
+        g2.fillRect(x, y, size, 10);
         g2.setColor(Color.white);
 
 
     }
 
-public void draw3(Graphics g2){
+
+    public void draw3(Graphics g2) {
         g2.setColor(Color.red);
         g2.fillRect(x, y, 10, height);
-}
+    }
 
-    public void collision(){
 
-        if(x < 100){
+    public void collision() {
+
+
+        if (x < 100) {
+            reset();
+        } else if (x > 580) {
+            reset();
+        } else if (y < 100) {
+            reset();
+        } else if (y > 580) {
             reset();
         }
-        else if(x > 580){
-            reset();
-        }
-        else if(y < 100){
-            reset();
-        }
-        else if(y > 580){
-            reset();
-        }
+
 
     }
-    public void reset(){
+    public void reset() {
         x = 300;
         y = 550;
     }
 
 
-    public void moveRight(){
-        for(int i = 0;i < 21;i++) {
+    public void moveRight() {
+        for (int i = 0; i < 21; i++) {
             x++;
         }
     }
-    public void moveLeft(){
-        for(int i = 0;i < 21;i++) {
+    public void moveLeft() {
+        for (int i = 0; i < 21; i++) {
             x--;
         }
 
+
     }
-    public void level1Restart(){
-        if(y == 400 && x < 400 && x > 100){
+    public void level1Restart() {
+        if (y == 400 && x < 400 && x > 100) {
             reset();
         }
     }
-    public void level1RestartAgain(){
-        if(y==400&& x >475){
+    public void level1RestartAgain() {
+        if (y == 400 && x > 475) {
             reset();
         }
     }
 
 
-    public void moves(){
-        y-=.5;
+    public void moves() {
+        y -= .5;
     }
 
-    public boolean restart(){
+
+    public boolean restart() {
+
 
         if (x < 350 && x > 175 && y == 105) {
 
+
             return true;
 
-        }
-        else{
+
+        } else {
             return false;
         }
     }
 
 
-    public boolean end2(){
-        if(x > 100 && y > 590){
+    public boolean end2() {
+        if (x > 100 && y > 590) {
             return true;
         }
         return false;
     }
 
 
-
-    public void boost(){
-        y-=10;
+    public void boost() {
+        y -= 10;
     }
     public void slow() {
-        y+=5;
+        y += 5;
     }
-    public void delete(){
-        x=1000;
-        y=1000;
+    public void delete() {
+        x = 1000;
+        y = 1000;
     }
-public void level2Restart(){
-        if(y==200&&(x<200||x >275)){
-            reset();
-    }
-}
-
-public void level3Restart(){
-        if(y==300 && x < 500){
+    public void level2Restart() {
+        if (y == 200 && (x < 200 || x > 275)) {
             reset();
         }
-}
-public void level4Restart(){
-        if(y==500&&x>175){
+    }
+
+
+    public void level3Restart() {
+        if (y == 300 && x < 500) {
             reset();
         }
-}
-public void pause(){
-        y+=1;
-}
-public void laser(){
-
-    if (y == 100) {
-        up = false;
     }
-    else if(y==600){
-        up = true;
-    }
-        if(up) {
-            y-=1.5;
+    public void level4Restart() {
+        if (y == 500 && x > 175) {
+            reset();
         }
-        else if(up == false){
-            y+=1.5;
+    }
+    public void pause() {
+        y += 1;
+    }
+    public void laser() {
+
+
+        if (y == 100 || y == 100 + 1 || y == 100 - 1 || y == 100 + 2|| y == 100 - 2) {
+            up = false;
+        } else if  (y == 600 || y == 600 + 1 || y == 600 - 1 || y == 600 + 2|| y == 600 - 2) {
+            up = true;
+        }
+        if (up) {
+            y -= 1.5;
+        } else if (up == false) {
+            y += 1.5;
         }
 
-}
-public void laser2(){
-        if(x==100){
+
+    }
+    public void laser2() {
+        if  (x == 100 || x == 100 + 1 || x == 100 - 1 || x == 100 + 2|| x == 100 - 2) {
             side = false;
-        }
-        else if(x==600){
+        } else if (x == 600 || x == 600 + 1 || x == 600 - 1 || x == 600 + 2|| x == 600 - 2) {
             side = true;
         }
-        if(side){
-            x-=1;
+        if (side) {
+            x -= 1;
+        } else if (side == false) {
+            x += 1;
         }
-        else if(side == false){
-            x+=1;
-        }
-}
-public int getY(){
+    }
+    public int getY() {
         return y;
-}
-public int getX(){
+    }
+    public int getX() {
         return x;
-}
+    }
+    public void changeBackground(){
+        int red = (int)(Math.random()*255+1);
+        int green = (int)(Math.random()*255+1);
+        int blue = (int)(Math.random()*255+1);
 
+    }
+    public void laser2Fast(){
+        if  (x == 100 || x == 100 + 1 || x == 100 - 1 || x == 100 + 2|| x == 100 - 2) {
+            side = false;
+        } else if (x == 600 || x == 600 + 1 || x == 600 - 1 || x == 600 + 2|| x == 600 - 2){
+            side = true;
+        }
+        if (side) {
+            x -= 4;
+        } else if (side == false) {
+            x += 4;
+        }
+    }
+    public void laserFast(){
+        if (y == 100 || y == 100 + 1 || y == 100 - 1 || y == 100 + 2|| y == 100 - 2) {
+            up = false;
+        } else if (y == 600 || y == 600 + 1 || y == 600 - 1 || y == 600 + 2|| y == 600 - 2) {
+            up = true;
+        }
+        if (up) {
+            y -= 4;
+        } else if (up == false) {
+            y += 4;
+        }
+    }
 
 
 }
